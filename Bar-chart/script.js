0792aa106607fd72fae4a1d4194ed916d4c9c21a
greatlalbabu <greatlalbabu.2015@gmail.com>
2024-02-03 23:02:56 +0530

    bar chart

--- Bar-chart/script.js
+++ Bar-chart/script.js
@@ -7,7 +7,10 @@ new Chart(ctx, {
     datasets: [{
       label: 'Monthly Sales',
       data: [80, 30, 130, 50, 100, 80],
+      backgroundColor: "rgba(100, 300, 30, 0.70)",
+
       borderWidth: 1
+
     }]
   },
   options: {
@@ -18,3 +21,20 @@ new Chart(ctx, {
     }
   }
 });
+
+
+new Chart(MY_CHART,CONFIG);
+
+anime({
+  targets:MY_CHART,
+  scale:1.5,
+  duration:3000,
+})
+
+setTimeout(()=>{
+  anime({
+    targets:MY_CHART,
+    scale:1,
+    duration:5000
+  })
+},3000);
